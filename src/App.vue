9ff9fedc92dfac764544f3c11fccd4ffc89ec58d
Kaarel Sirel <sirelkaarel@gmail.com>
2025-02-28 21:40:06 +0200

    Added basic functionality to complete, restart and delete tasks

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,45 @@
 <script setup lang="ts">
 import TaskItem from "./components/TaskItem.vue";
+import {useId, ref} from "vue";
+
+
+export interface Task {
+  id: string;
+  name: string;
+  isCompleted: boolean;
+}
+
+let tasks = ref<Task[]>([
+  {'id': useId(), 'name': 'Feed the cat', 'isCompleted': true},
+  {'id': useId(), 'name': 'Water the flowers', 'isCompleted': false},
+  {'id': useId(), 'name': 'Take out the trash', 'isCompleted': false},
+  {'id': useId(), 'name': 'Complete the tasks', 'isCompleted': true},
+]);
+
+const completeTask = (id: string) => {
+  const taskIndex = tasks.value.findIndex((task) => task.id === id);
+
+  if (taskIndex !== -1) {
+    tasks.value[taskIndex].isCompleted = true;
+  }
+}
+
+const restartTask = (id: string) => {
+  const taskIndex = tasks.value.findIndex((task) => task.id === id);
+
+  if (taskIndex !== -1) {
+    tasks.value[taskIndex].isCompleted = false;
+  }
+}
+
+const removeTask = (id: string) => {
+  const taskIndex = tasks.value.findIndex((task) => task.id === id);
+
+  if (taskIndex !== -1) {
+    tasks.value.splice(taskIndex, 1);
+  }
+}
+
 </script>
 
 <template>
@@ -8,9 +48,16 @@ import TaskItem from "./components/TaskItem.vue";
       <div class="card-body">
         <h2 class="card-title font-bold text-3xl">My Tasks</h2>
         <div class="flex flex-col gap-2 mt-3">
-          <TaskItem />
-          <TaskItem />
-          <TaskItem />
+          <TaskItem
+              v-if="tasks.length > 0"
+              v-for="task in tasks"
+              :key="task.id"
+              :task
+              @complete-task="completeTask"
+              @restart-task="restartTask"
+              @remove-task="removeTask"
+          />
+          <h1 v-else>You have no tasks.</h1>
         </div>
       </div>
     </div>
--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -1,18 +1,25 @@
 <script setup lang="ts">
+import { Trash, Check, RotateCcw } from "lucide-vue-next";
+import type { Task } from "../App.vue";
 
-import {Trash, Check} from "lucide-vue-next";
+defineProps<{
+  task: Task;
+}>();
 </script>
 
 <template>
   <div class="card card-border bg-base-100 w-full">
     <div class="card-body">
       <div class="flex justify-between">
-        <h1 class="text-2xl font-bold">Task Name</h1>
+        <h1 class="text-xl font-bold" :class="{ 'line-through': task.isCompleted }">{{ task.name }}</h1>
         <div class="flex gap-2">
-          <button class="btn btn-square btn-success btn-sm">
+          <button v-if="task.isCompleted" class="btn btn-square btn-warning btn-sm" @click="$emit('restart-task', task.id)">
+            <RotateCcw />
+          </button>
+          <button v-else  class="btn btn-square btn-success btn-sm" @click="$emit('complete-task', task.id)">
             <Check />
           </button>
-          <button class="btn btn-square btn-error btn-sm">
+          <button class="btn btn-square btn-error btn-sm" @click="$emit('remove-task', task.id)">
             <Trash />
           </button>
         </div>
