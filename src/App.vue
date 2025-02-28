d183306c0fed1f128fc2abe8e4754a3c5028f33f
Kaarel Sirel <sirelkaarel@gmail.com>
2025-02-28 20:46:00 +0200

    Initial layout

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
-
+import TaskItem from "./components/TaskItem.vue";
 </script>
 
 <template>
-  <div class="flex justify-center h-full">
-    <p>test</p>
+  <div class="flex justify-center items-center h-screen">
+    <div class="card card-border bg-base-100 w-96">
+      <div class="card-body">
+        <h2 class="card-title font-bold text-3xl">My Tasks</h2>
+        <div class="flex flex-col gap-2 mt-3">
+          <TaskItem />
+          <TaskItem />
+          <TaskItem />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
---
+++ src/components/TaskItem.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+import {Trash, Check} from "lucide-vue-next";
+</script>
+
+<template>
+  <div class="card card-border bg-base-100 w-full">
+    <div class="card-body">
+      <div class="flex justify-between">
+        <h1 class="text-2xl font-bold">Task Name</h1>
+        <div class="flex gap-2">
+          <button class="btn btn-square btn-success btn-sm">
+            <Check />
+          </button>
+          <button class="btn btn-square btn-error btn-sm">
+            <Trash />
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
