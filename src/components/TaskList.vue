1cbfefffa64ef22bdae725c55021dcedce0f90b9
Kaarel Sirel <sirelkaarel@gmail.com>
2025-02-28 23:10:19 +0200

    Refactor: move list of tasks to a separate component TaskList.vue

---
+++ src/components/TaskList.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+
+import { Plus } from "lucide-vue-next";
+import { ref } from "vue";
+import { Task } from "@types/index";
+import TaskListItem from "./TaskListItem.vue";
+
+defineProps<{
+  tasks: Task[]
+}>()
+
+const completeTask = (id: string) => {
+  const taskIndex = tasks.value.findIndex((task) => task.id === id);
+
+  if (taskIndex !== -1) {
+    tasks.value[taskIndex].isCompleted = true;
+  }
+}
+
+const restartTask = (id: string) => {
+  const taskIndex = tasks.value.findIndex((task) => task.id === id);
+
+  if (taskIndex !== -1) {
+    tasks.value[taskIndex].isCompleted = false;
+  }
+}
+
+const removeTask = (id: string) => {
+  const taskIndex = tasks.value.findIndex((task) => task.id === id);
+
+  if (taskIndex !== -1) {
+    tasks.value.splice(taskIndex, 1);
+  }
+}
+
+const addNewTask = (name: string) => {
+  tasks.value.push({'id': crypto.randomUUID(), 'name': name, 'isCompleted': false})
+  newTaskName.value = '';
+}
+
+let newTaskName = ref<string>('');
+</script>
+
+<template>
+  <div class="card card-border bg-base-100 w-96">
+    <div class="card-body">
+      <div class="mb-2">
+        <h2 class="card-title font-bold text-3xl">My Tasks</h2>
+      </div>
+      <div class="join w-full">
+        <label class="input join-item">
+          <input type="text" placeholder="What do you need to do?" required v-model="newTaskName" />
+        </label>
+        <button class="btn join-item" @click="addNewTask(newTaskName)">
+          <Plus/>
+        </button>
+      </div>
+      <div class="flex flex-col gap-2 mt-3">
+        <TaskListItem
+            v-if="tasks.length > 0"
+            v-for="task in tasks"
+            :key="task.id"
+            :task
+            @complete-task="completeTask"
+            @restart-task="restartTask"
+            @remove-task="removeTask"
+        />
+        <h1 v-else>You have no tasks.</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/components/TaskListItem.vue
+++ src/components/TaskListItem.vue
@@ -10,13 +10,13 @@ defineProps<{
 <template>
   <div class="card card-border bg-base-100 w-full">
     <div class="card-body">
-      <div class="flex justify-between">
+      <div class="flex justify-between items-center">
         <h1 class="text-xl font-bold" :class="{ 'line-through': task.isCompleted }">{{ task.name }}</h1>
         <div class="flex gap-2">
           <button v-if="task.isCompleted" class="btn btn-square btn-warning btn-sm" @click="$emit('restart-task', task.id)">
             <RotateCcw />
           </button>
-          <button v-else  class="btn btn-square btn-success btn-sm" @click="$emit('complete-task', task.id)">
+          <button v-else class="btn btn-square btn-success btn-sm" @click="$emit('complete-task', task.id)">
             <Check />
           </button>
           <button class="btn btn-square btn-error btn-sm" @click="$emit('remove-task', task.id)">
@@ -27,7 +27,3 @@ defineProps<{
     </div>
   </div>
 </template>
-
-<style scoped>
-
-</style>
